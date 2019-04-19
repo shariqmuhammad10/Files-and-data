0a4aa2026a4027b75a094b34f279f1cf7ff60f3e
Shariq Muhammad <smuhammad@CS.NET>
2019-04-19 16:58:13 +0500

    batch files adding

---
+++ copyRepoToFolder.bat
@@ -0,0 +1,12 @@
+@echo off 
+
+echo "move to D drive"
+
+d:
+
+echo "Access the folder where REPO need to be copied"
+cd D:\BuildFromBIT\AAA\JavaProject
+
+echo "Copy from Repository to Desired Folder"
+git pull 
+
